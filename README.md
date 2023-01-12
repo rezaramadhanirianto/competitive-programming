43633deab98c70f3fa4f18d762b7f30b22106788
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2023-01-12 13:53:29 +0700

    add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Competitive Programming
+
+This repository contains my solutions for competitive programming contest. Maybe all solutions in this repository not accepted.
--- src/main/java/practice/pastzco/SmartPhone.java
+++ src/main/java/practice/pastzco/SmartPhone.java
@@ -1,2 +1,23 @@
-package practice.pastzco;public class SmartPhone {
+package practice.pastzco;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// https://www.codechef.com/ZCOPRAC/problems/ZCO14003
+public class SmartPhone {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[i] = scanner.nextInt();
+
+        long res = 0l;
+        Arrays.sort(arr);
+        for(int i = 0; i < n; i++){
+            res = (long) Math.max(res, ((long) arr[i]*(n-i)));
+        }
+
+        System.out.println(res);
+    }
 }
