8ae90a843940a3adf55f81985389ed33df7edcb8
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2023-05-07 00:56:07 +0700

    Add template

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,61 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
-// template
-public class Main {
+public class Main implements Runnable {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st = new StringTokenizer("");
+
     public static void main(String[] args) {
+        new Thread(null, new Main(), "Main", 1<<28).start();
+    }
 
+    @Override
+    public void run() {
+        int tests = readInt();
+
+        while(tests-- > 0){
+
+        }
+        out.close();
+    }
+
+    public static String next() {
+        try {
+            while (!st.hasMoreTokens()) {
+                String s = br.readLine();
+                if (s == null)
+                    return null;
+                st = new StringTokenizer(s);
+            }
+            return st.nextToken();
+        } catch(Exception e)  {
+            return  null;
+        }
+    }
+
+    static long readLong() {
+        return Long.parseLong(next());
+    }
+
+    static int readInt() {
+        return Integer.parseInt(next());
+    }
+
+    static double readDouble() {
+        return Double.parseDouble(next());
+    }
+
+    static char readCharacter() {
+        return next().charAt(0);
+    }
+
+    static String readLine() throws IOException {
+        return br.readLine().trim();
     }
 }
