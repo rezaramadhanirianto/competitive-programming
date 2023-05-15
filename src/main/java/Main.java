23cfe2e443b4d7e0ba2c6ea8cc6f53db084e01b5
Reza Ramadhan Irianto <46983732+rezaramadhanirianto@users.noreply.github.com>
2023-05-15 10:35:31 +0700

    fix import in template

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main implements Runnable {
